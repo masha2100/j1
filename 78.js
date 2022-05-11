c04a9eef77eb08590e3b16875a0b38e8d8f66d36
Maria <ruzxkovam18@gmail.com>
2022-05-11 18:38:31 +0300

    homework

---
+++ 78.js
@@ -0,0 +1,13 @@
+//№1
+let a = prompt("площадь круга:")
+let b = 3.14
+alert(a*b)
+//№2
+let a1 = prompt("year:")
+let b2 = 2022
+alert(b2-a1)
+//3
+let a2 = prompt("how old are you?:")
+let b3 = 65
+let c = 2022 
+alert(b3 - a2,c + (b3-a2))
